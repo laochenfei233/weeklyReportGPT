c9c604c25ee45977b12fe8b54a422acd4675da01
laochenfei233 <laochenfei233@limpet.cn>
2025-08-24 23:43:27 +0800

    Update register.ts

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -94,7 +94,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       user: {
         id: user.id,
         email: user.email,
-        username: user.username,
+        username: user.username || username,
         isAdmin: user.is_admin
       },
       token
